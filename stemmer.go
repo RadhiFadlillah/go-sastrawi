df5330529952582e8d7a1279574c0b056b348c97
Radhi Fadlillah <m.radhi.f@gmail.com>
2016-09-25 06:06:26 +0700

    Implement infix removal, loopPengembalianAkhiran and fix some rules

--- stemmer.go
+++ stemmer.go
@@ -7,12 +7,12 @@ import (
 
 const (
 	vowel     = "aiueo"
-	consonant = "bcdfghjklmnpqrsuvwxyz"
+	consonant = "bcdfghjklmnpqrstvwxyz"
 )
 
 type char string
 
-func NewChar(word string, index int) char {
+func newChar(word string, index int) char {
 	if index >= len(word) {
 		return char("")
 	}
@@ -20,7 +20,7 @@ func NewChar(word string, index int) char {
 	return char(word[index])
 }
 
-func (c char) Is(chars string) bool {
+func (c char) is(chars string) bool {
 	for _, char := range strings.Split(chars, "") {
 		if string(c) == char {
 			return true
@@ -30,13 +30,12 @@ func (c char) Is(chars string) bool {
 	return false
 }
 
-func (c char) IsNot(chars string) bool {
-	return !c.Is(chars)
+func (c char) isNot(chars string) bool {
+	return !c.is(chars)
 }
 
 type Stemmer struct {
 	dictionary    Dictionary
-	rxDisallowed  *regexp.Regexp
 	rxPrefixFirst *regexp.Regexp
 	rxParticle    *regexp.Regexp
 	rxPossesive   *regexp.Regexp
@@ -46,7 +45,6 @@ type Stemmer struct {
 func NewStemmer(dict Dictionary) *Stemmer {
 	return &Stemmer{
 		dictionary:    dict,
-		rxDisallowed:  regexp.MustCompile(`^(ber.+i|di.+an|ke.+i|ke.+kan|me.+an|ter.+an|per.+an)$`),
 		rxPrefixFirst: regexp.MustCompile(`^(be.+lah|be.+an|me.+i|di.+i|pe.+i|ter.+i)$`),
 		rxParticle:    regexp.MustCompile(`-?(lah|kah|tah|pun)$`),
 		rxPossesive:   regexp.MustCompile(`-?(ku|mu|nya)$`),
@@ -55,9 +53,13 @@ func NewStemmer(dict Dictionary) *Stemmer {
 }
 
 func (stemmer *Stemmer) Stem(word string) string {
-	rootFound := false
-	originalWord := word
-	recodingChar := []string{}
+	var (
+		rootFound    = false
+		originalWord = word
+		particle     string
+		possesive    string
+		suffix       string
+	)
 
 	if len(word) < 3 {
 		return word
@@ -70,71 +72,115 @@ func (stemmer *Stemmer) Stem(word string) string {
 	// Check if prefix must be removed first
 	if stemmer.rxPrefixFirst.MatchString(word) {
 		// Remove prefix
-		rootFound, word, recodingChar = stemmer.removePrefixes(word)
-		if rootFound {
-			return word
-		}
-
-		// Try recoding
-		rootFound, word = stemmer.recodingPrefix(word, recodingChar)
+		rootFound, word = stemmer.removePrefixes(word)
 		if rootFound {
 			return word
 		}
 
 		// Remove particle
-		word = stemmer.rxParticle.ReplaceAllString(word, "")
+		particle, word = stemmer.removeParticle(word)
 		if stemmer.dictionary.Find(word) {
 			return word
 		}
 
 		// Remove possesive
-		word = stemmer.rxPossesive.ReplaceAllString(word, "")
+		possesive, word = stemmer.removePossesive(word)
 		if stemmer.dictionary.Find(word) {
 			return word
 		}
 
 		// Remove suffix
-		word = stemmer.rxSuffix.ReplaceAllString(word, "")
+		suffix, word = stemmer.removeSuffix(word)
 		if stemmer.dictionary.Find(word) {
 			return word
 		}
 	} else {
 		// Remove particle
-		word = stemmer.rxParticle.ReplaceAllString(word, "")
+		particle, word = stemmer.removeParticle(word)
 		if stemmer.dictionary.Find(word) {
 			return word
 		}
 
 		// Remove possesive
-		word = stemmer.rxPossesive.ReplaceAllString(word, "")
+		possesive, word = stemmer.removePossesive(word)
 		if stemmer.dictionary.Find(word) {
 			return word
 		}
 
 		// Remove suffix
-		word = stemmer.rxSuffix.ReplaceAllString(word, "")
+		suffix, word = stemmer.removeSuffix(word)
 		if stemmer.dictionary.Find(word) {
 			return word
 		}
 
 		// Remove prefix
-		rootFound, word, recodingChar = stemmer.removePrefixes(word)
+		rootFound, word = stemmer.removePrefixes(word)
 		if rootFound {
 			return word
 		}
+	}
 
-		// Try recoding
-		rootFound, word = stemmer.recodingPrefix(word, recodingChar)
-		if rootFound {
-			return word
-		}
+	// If no root found, do loopPengembalianAkhiran
+	removedSuffixes := []string{"", suffix, possesive, particle}
+	if suffix == "kan" {
+		removedSuffixes = []string{"", "k", "an", possesive, particle}
+	}
+
+	rootFound, word = stemmer.loopPengembalianAkhiran(originalWord, removedSuffixes)
+	if rootFound {
+		return word
 	}
 
 	// When EVERYTHING failed, return original word
 	return originalWord
 }
 
-func (stemmer *Stemmer) removePrefixes(word string) (bool, string, []string) {
+func (stemmer *Stemmer) removeParticle(word string) (string, string) {
+	result := stemmer.rxParticle.ReplaceAllString(word, "")
+	particle := strings.Replace(word, result, "", 1)
+	return particle, result
+}
+
+func (stemmer *Stemmer) removePossesive(word string) (string, string) {
+	result := stemmer.rxPossesive.ReplaceAllString(word, "")
+	possesive := strings.Replace(word, result, "", 1)
+	return possesive, result
+}
+
+func (stemmer *Stemmer) removeSuffix(word string) (string, string) {
+	result := stemmer.rxSuffix.ReplaceAllString(word, "")
+	suffix := strings.Replace(word, result, "", 1)
+	return suffix, result
+}
+
+func (stemmer *Stemmer) loopPengembalianAkhiran(originalWord string, suffixes []string) (bool, string) {
+	lenSuffixes := 0
+	for _, suffix := range suffixes {
+		lenSuffixes += len(suffix)
+	}
+	wordWithoutSuffix := originalWord[:len(originalWord)-lenSuffixes]
+
+	for i := range suffixes {
+		suffixCombination := ""
+		for j := 0; j <= i; j++ {
+			suffixCombination += suffixes[j]
+		}
+
+		word := wordWithoutSuffix + suffixCombination
+		if stemmer.dictionary.Find(word) {
+			return true, word
+		}
+
+		rootFound, word := stemmer.removePrefixes(word)
+		if rootFound {
+			return true, word
+		}
+	}
+
+	return false, originalWord
+}
+
+func (stemmer *Stemmer) removePrefixes(word string) (bool, string) {
 	originalWord := word
 	currentPrefix := ""
 	removedPrefix := ""
@@ -142,11 +188,7 @@ func (stemmer *Stemmer) removePrefixes(word string) (bool, string, []string) {
 
 	for i := 0; i < 3; i++ {
 		if len(word) < 3 {
-			return false, originalWord, nil
-		}
-
-		if stemmer.rxDisallowed.MatchString(word) {
-			break
+			return false, originalWord
 		}
 
 		currentPrefix = word[:2]
@@ -156,21 +198,13 @@ func (stemmer *Stemmer) removePrefixes(word string) (bool, string, []string) {
 
 		removedPrefix, word, recodingChar = stemmer.removePrefix(word)
 		if stemmer.dictionary.Find(word) {
-			return true, word, recodingChar
+			return true, word
 		}
-	}
 
-	return false, word, recodingChar
-}
-
-func (stemmer *Stemmer) recodingPrefix(word string, recodingChar []string) (bool, string) {
-	if recodingChar == nil {
-		return false, word
-	}
-
-	for _, char := range recodingChar {
-		if stemmer.dictionary.Find(char + word) {
-			return true, char + word
+		for _, char := range recodingChar {
+			if stemmer.dictionary.Find(char + word) {
+				return true, char + word
+			}
 		}
 	}
 
@@ -179,97 +213,106 @@ func (stemmer *Stemmer) recodingPrefix(word string, recodingChar []string) (bool
 
 func (stemmer *Stemmer) removePrefix(word string) (string, string, []string) {
 	var (
-		prefix   string   = word[:2]
-		recoding []string = nil
+		prefix   string
 		result   string
+		recoding []string = nil
 	)
 
-	switch prefix {
-	case "di":
-		fallthrough
-	case "ke":
-		fallthrough
-	case "se":
-		fallthrough
-	case "ku":
+	if strings.HasPrefix(word, "di") || strings.HasPrefix(word, "ke") || strings.HasPrefix(word, "se") || strings.HasPrefix(word, "ku") {
+		prefix = word[:2]
 		result = word[2:]
-	case "me":
+	} else if strings.HasPrefix(word, "kau") {
+		prefix = "kau"
+		result = word[3:]
+	} else if strings.HasPrefix(word, "me") {
+		prefix = "me"
 		result, recoding = stemmer.removeMePrefix(word)
-	case "pe":
+	} else if strings.HasPrefix(word, "pe") {
+		prefix = "pe"
 		result, recoding = stemmer.removePePrefix(word)
-	case "be":
+	} else if strings.HasPrefix(word, "be") {
+		prefix = "be"
 		result, recoding = stemmer.removeBePrefix(word)
-	case "te":
+	} else if strings.HasPrefix(word, "te") {
+		prefix = "te"
 		result, recoding = stemmer.removeTePrefix(word)
-	default:
-		result = word
+	} else {
+		result, recoding = stemmer.removeInfix(word)
 	}
 
 	return prefix, result, recoding
 }
 
 func (stemmer *Stemmer) removeMePrefix(word string) (string, []string) {
-	s3 := NewChar(word, 2)
-	s4 := NewChar(word, 3)
-	s5 := NewChar(word, 4)
+	s3 := newChar(word, 2)
+	s4 := newChar(word, 3)
+	s5 := newChar(word, 4)
 
 	// Pattern 01
 	// me[lrwy][aiueo] => [lrwy][aiueo]
-	if s3.Is("lrwy") && s4.Is(vowel) {
+	if s3.is("lrwy") && s4.is(vowel) {
 		return word[2:], nil
 	}
 
 	// Pattern 02
 	// mem[bfv] => [bfv]
-	if s3.Is("m") && s4.Is("bfv") {
+	if s3.is("m") && s4.is("bfv") {
 		return word[3:], nil
 	}
 
 	// Pattern 03
 	// mempe => pe
-	if s3.Is("m") && s4.Is("p") && s5.Is("e") {
+	if s3.is("m") && s4.is("p") && s5.is("e") {
 		return word[3:], nil
 	}
 
 	// Pattern 04
 	// mem(r?)[aiueo] => m(r?)[aiueo] OR p(r?)[aiueo]
-	if s3.Is("m") && (s4.Is(vowel) || (s4.Is("r") && s5.Is(vowel))) {
+	if s3.is("m") && (s4.is(vowel) || (s4.is("r") && s5.is(vowel))) {
 		return word[3:], []string{"m", "p"}
 	}
 
 	// Pattern 05
 	// men[cdjstz] => [cdjstz]
-	if s3.Is("n") && s4.Is("cdjstz") {
+	if s3.is("n") && s4.is("cdjstz") {
 		return word[3:], nil
 	}
 
 	// Pattern 06
 	// men[aiueo] => n[aiueo] OR t[aiueo]
-	if s3.Is("n") && s4.Is(vowel) {
+	if s3.is("n") && s4.is(vowel) {
 		return word[3:], []string{"n", "t"}
 	}
 
 	// Pattern 07
 	// meng[ghqk] => [ghqk]
-	if s3.Is("n") && s4.Is("g") && s5.Is("ghqk") {
+	if s3.is("n") && s4.is("g") && s5.is("ghqk") {
 		return word[4:], nil
 	}
 
 	// Pattern 08
 	// meng[aiueo] => [aiueo]
-	if s3.Is("n") && s4.Is("g") && s5.Is(vowel) {
-		return word[4:], []string{"k"}
+	if s3.is("n") && s4.is("g") && s5.is(vowel) {
+		if s5.is("e") {
+			return word[5:], nil
+		}
+
+		return word[4:], []string{"ng", "k"}
 	}
 
 	// Pattern 09
 	// meny[aiueo] => s[aiueo]
-	if s3.Is("n") && s4.Is("y") && s5.Is(vowel) {
+	if s3.is("n") && s4.is("y") && s5.is(vowel) {
+		if s5.is("a") {
+			return word[2:], nil
+		}
+
 		return "s" + word[4:], nil
 	}
 
 	// Pattern 10
-	// memp[aiuo] => p[aiuo]
-	if s3.Is("m") && s4.Is("p") && s5.Is("e") {
+	// memp[^e] => p[^e]
+	if s3.is("m") && s4.is("p") && s5.isNot("e") {
 		return word[3:], nil
 	}
 
@@ -277,71 +320,71 @@ func (stemmer *Stemmer) removeMePrefix(word string) (string, []string) {
 }
 
 func (stemmer *Stemmer) removePePrefix(word string) (string, []string) {
-	s3 := NewChar(word, 2)
-	s4 := NewChar(word, 3)
-	s5 := NewChar(word, 4)
-	s6 := NewChar(word, 5)
-	s7 := NewChar(word, 6)
-	s8 := NewChar(word, 7)
+	s3 := newChar(word, 2)
+	s4 := newChar(word, 3)
+	s5 := newChar(word, 4)
+	s6 := newChar(word, 5)
+	s7 := newChar(word, 6)
+	s8 := newChar(word, 7)
 
 	// Pattern 01
 	// pe[wy][aiueo] => [wy][aiueo]
-	if s3.Is("wy") && s4.Is(vowel) {
+	if s3.is("wy") && s4.is(vowel) {
 		return word[2:], nil
 	}
 
 	// Pattern 02
 	// per[aiueo] => [aiueo] OR r[aiueo]
-	if s3.Is("r") && s4.Is(vowel) {
+	if s3.is("r") && s4.is(vowel) {
 		return word[3:], []string{"r"}
 	}
 
 	// Pattern 03
 	// per[^aiueor][a-z][^e] => [^aiueor][a-z][^e]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.IsNot("") && s6.IsNot("e") {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.isNot("") && s6.isNot("e") {
 		return word[3:], nil
 	}
 
 	// Pattern 4
 	// per[^aiueor][a-z]er[aiueo] => [^aiueor][a-z]er[aiueo]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.IsNot("") && s6.Is("e") && s7.Is("r") && s8.Is(vowel) {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.isNot("") && s6.is("e") && s7.is("r") && s8.is(vowel) {
 		return word[3:], nil
 	}
 
 	// Pattern 05
 	// pem[bfv] => [bfv]
-	if s3.Is("m") && s4.Is("bfv") {
+	if s3.is("m") && s4.is("bfv") {
 		return word[3:], nil
 	}
 
 	// Pattern 06
 	// pem(r?)[aiueo] => m(r?)[aiueo] OR p(r?)[aiueo]
-	if s3.Is("m") && (s4.Is(vowel) || (s4.Is("r") && s5.Is(vowel))) {
+	if s3.is("m") && (s4.is(vowel) || (s4.is("r") && s5.is(vowel))) {
 		return word[3:], []string{"m", "p"}
 	}
 
 	// Pattern 07
 	// pen[cdjz] => [cdjz]
-	if s3.Is("n") && s4.Is("cdjz") {
+	if s3.is("n") && s4.is("cdjz") {
 		return word[3:], nil
 	}
 
 	// Pattern 08
 	// pen[aiueo] => n[aiueo] OR t[aiueo]
-	if s3.Is("n") && s4.Is(vowel) {
+	if s3.is("n") && s4.is(vowel) {
 		return word[3:], []string{"n", "t"}
 	}
 
 	// Pattern 09
 	// peng[^aiueo] => [^aiueo]
-	if s3.Is("n") && s4.Is("g") && s5.Is(consonant) {
+	if s3.is("n") && s4.is("g") && s5.is(consonant) {
 		return word[4:], nil
 	}
 
 	// Pattern 10
 	// peng[aiueo] => [aiueo] OR k[aiueo]
-	if s3.Is("n") && s4.Is("g") && s5.Is(vowel) {
-		if s5.Is("e") {
+	if s3.is("n") && s4.is("g") && s5.is(vowel) {
+		if s5.is("e") {
 			return word[5:], nil
 		}
 
@@ -350,13 +393,17 @@ func (stemmer *Stemmer) removePePrefix(word string) (string, []string) {
 
 	// Pattern 11
 	// peny[aiueo] => s[aiueo]
-	if s3.Is("n") && s4.Is("y") && s5.Is(vowel) {
+	if s3.is("n") && s4.is("y") && s5.is(vowel) {
+		if s5.is("a") {
+			return word[2:], nil
+		}
+
 		return "s" + word[4:], nil
 	}
 
 	// Pattern 12
 	// pel[aiueo] => l[aiueo] OR 'ajar' for 'pelajar'
-	if s3.Is("l") && s4.Is(vowel) {
+	if s3.is("l") && s4.is(vowel) {
 		if word == "pelajar" {
 			return "ajar", nil
 		}
@@ -366,19 +413,19 @@ func (stemmer *Stemmer) removePePrefix(word string) (string, []string) {
 
 	// Pattern 13
 	// pe[^aiueorwylmn]er[aiueo] => er[aiueo]
-	if s3.Is(consonant) && s3.IsNot("rwylmn") && s4.Is("e") && s5.Is("r") && s6.Is(vowel) {
+	if s3.is(consonant) && s3.isNot("rwylmn") && s4.is("e") && s5.is("r") && s6.is(vowel) {
 		return word[3:], nil
 	}
 
 	// Pattern 14
 	// pe[^aiueorwylmn][^e] => [^aiueorwylmn][^e]
-	if s3.Is(consonant) && s3.IsNot("rwylmn") && s4.IsNot("e") {
+	if s3.is(consonant) && s3.isNot("rwylmn") && s4.isNot("e") {
 		return word[2:], nil
 	}
 
 	// Pattern 15
 	// pe[^aiueorwylmn]er[^aiueo] => [^aiueorwylmn]er[^aiueo]
-	if s3.Is(consonant) && s3.IsNot("rwylmn") && s4.Is("e") && s5.Is("r") && s6.Is(consonant) {
+	if s3.is(consonant) && s3.isNot("rwylmn") && s4.is("e") && s5.is("r") && s6.is(consonant) {
 		return word[2:], nil
 	}
 
@@ -386,28 +433,28 @@ func (stemmer *Stemmer) removePePrefix(word string) (string, []string) {
 }
 
 func (stemmer *Stemmer) removeBePrefix(word string) (string, []string) {
-	s3 := NewChar(word, 2)
-	s4 := NewChar(word, 3)
-	s5 := NewChar(word, 4)
-	s6 := NewChar(word, 5)
-	s7 := NewChar(word, 6)
-	s8 := NewChar(word, 7)
+	s3 := newChar(word, 2)
+	s4 := newChar(word, 3)
+	s5 := newChar(word, 4)
+	s6 := newChar(word, 5)
+	s7 := newChar(word, 6)
+	s8 := newChar(word, 7)
 
 	// Pattern 01
 	// ber[aiueo] => [aiueo] OR r[aiueo]
-	if s3.Is("r") && s4.Is(vowel) {
+	if s3.is("r") && s4.is(vowel) {
 		return word[3:], []string{"r"}
 	}
 
 	// Pattern 02
 	// ber[^aiueor][a-z][^e] => [^aiueor][a-z][^e]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.IsNot("") && s6.IsNot("e") {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.isNot("") && s6.isNot("e") {
 		return word[3:], nil
 	}
 
 	// Pattern 3
 	// ber[^aiueor][a-z]er[aiueo] => [^aiueor][a-z]er[aiueo]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.IsNot("") && s6.Is("e") && s7.Is("r") && s8.Is(vowel) {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.isNot("") && s6.is("e") && s7.is("r") && s8.is(vowel) {
 		return word[3:], nil
 	}
 
@@ -418,8 +465,8 @@ func (stemmer *Stemmer) removeBePrefix(word string) (string, []string) {
 	}
 
 	// Pattern 5
-	// be[^aiueorl]er[aiueo] => [^aiueorl]er[aiueo]
-	if s3.Is(consonant) && s3.IsNot("r") && s3.IsNot("l") && s4.Is("e") && s5.Is("r") && s6.Is(vowel) {
+	// be[^aiueorl]er[^aiueo] => [^aiueorl]er[^aiueo]
+	if s3.is(consonant) && s3.isNot("r") && s3.isNot("l") && s4.is("e") && s5.is("r") && s6.is(consonant) {
 		return word[2:], nil
 	}
 
@@ -427,40 +474,61 @@ func (stemmer *Stemmer) removeBePrefix(word string) (string, []string) {
 }
 
 func (stemmer *Stemmer) removeTePrefix(word string) (string, []string) {
-	s3 := NewChar(word, 2)
-	s4 := NewChar(word, 3)
-	s5 := NewChar(word, 4)
-	s6 := NewChar(word, 5)
-	s7 := NewChar(word, 6)
+	s3 := newChar(word, 2)
+	s4 := newChar(word, 3)
+	s5 := newChar(word, 4)
+	s6 := newChar(word, 5)
+	s7 := newChar(word, 6)
 
 	// Pattern 01
 	// ter[aiueo] => [aiueo] OR r[aiueo]
-	if s3.Is("r") && s4.Is(vowel) {
+	if s3.is("r") && s4.is(vowel) {
 		return word[3:], []string{"r"}
 	}
 
 	// Pattern 02
 	// ter[^aiueor]er[aiueo] => [^aiueor]er[aiueo]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.Is("e") && s6.Is("r") && s7.Is(vowel) {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.is("e") && s6.is("r") && s7.is(vowel) {
 		return word[3:], nil
 	}
 
 	// Pattern 3
 	// ter[^aiueor][^e] => [^aiueor][^e]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.IsNot("e") {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.isNot("e") {
 		return word[3:], nil
 	}
 
 	// Pattern 04
 	// te[^aiueor]er[^aiueo] => [^aiueor]er[^aiueo]
-	if s3.Is(consonant) && s3.IsNot("r") && s4.Is("e") && s5.Is("r") && s6.Is(consonant) {
+	if s3.is(consonant) && s3.isNot("r") && s4.is("e") && s5.is("r") && s6.is(consonant) {
 		return word[2:], nil
 	}
 
 	// Pattern 05
 	// ter[^aiueor]er[^aiueo] => [^aiueor]er[^aiueo]
-	if s3.Is("r") && s4.Is(consonant) && s4.IsNot("r") && s5.Is("e") && s6.Is("r") && s7.Is(consonant) {
+	if s3.is("r") && s4.is(consonant) && s4.isNot("r") && s5.is("e") && s6.is("r") && s7.is(consonant) {
 		return word[3:], nil
+	}
+
+	return word, nil
+}
+
+func (stemmer *Stemmer) removeInfix(word string) (string, []string) {
+	s1 := newChar(word, 0)
+	s2 := newChar(word, 1)
+	s3 := newChar(word, 2)
+	s4 := newChar(word, 3)
+
+	// Pattern 01
+	// CerV => CerV OR CV
+	if s1.is(consonant) && s2.is("e") && s3.is("rlm") && s4.is(vowel) {
+		return word[3:], []string{word[:3], word[:1]}
+	}
+
+	// Pattern 02
+	// CinV => CinV OR CV
+	if s1.is(consonant) && s2.is("i") && s3.is("n") && s4.is(vowel) {
+		return word[3:], []string{word[:3], word[:1]}
 	}
 
 	return word, nil
